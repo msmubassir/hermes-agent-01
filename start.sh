1b7a1d5786e6cab0048f62d9fcb8a42b1a32bca1
msmubassir <125449220+msmubassir@users.noreply.github.com>
2026-05-15 07:39:48 +0600

    Change ttyd installation path to /opt/data

--- start.sh
+++ start.sh
@@ -11,13 +11,18 @@ hermes dashboard \
   --no-open &
   
 # Download ttyd if missing
-if [ ! -f /usr/local/bin/ttyd ]; then
+if [ ! -f /opt/data/ttyd ]; then
   curl -L \
     https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x86_64 \
-    -o /usr/local/bin/ttyd
+    -o /opt/data/ttyd
 
-  chmod +x /usr/local/bin/ttyd
+  chmod +x /opt/data/ttyd
 fi
 
+# Web terminal
+/opt/data/ttyd \
+  --port 7681 \
+  bash &
+  
 # Keep container alive
 sleep infinity
